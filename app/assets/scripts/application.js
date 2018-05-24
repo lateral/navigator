4b2aba627e1a52fb24620780bea60e098c6a2c8c
Max Novakovic <max@lateral.io>
2018-05-24 14:49:50 +0200

    Use new cookie message

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -1,14 +1,5 @@
 var Router = require('./routers/root_router');
 
-// Standard Google Universal Analytics code
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-45843453-10', 'auto');
-ga('set', 'checkProtocolTask', function(){});
-ga('require', 'displayfeatures');
-
 window.jQuery = $;
 window.App = new Backbone.Marionette.Application();
 
